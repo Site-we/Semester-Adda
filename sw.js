813348f32b944a17124fce35c53f7c09b977b3ca
Nikhil Kumar <nikhilkumar.storage@gmail.com>
2025-09-14 23:09:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener("fetch", (event) => {
             const allowedUserAgent = "Mozilla/5.0 (Linux; Android 15; GhostDevice 99) AppleWebKit/999.99 (KHTML, like Gecko) PhantomBrowser/10.0 Mobile Safari/999.99";
             
             if (userAgent !== allowedUserAgent) {
-                return new Response("<h2>Access Denied</h2><p>This webpage can only be viewed inside the Gtech-poly Unofficial app.</p>", {
+                return new Response("<h2>Access Denied</h2><p>This webpage can only be viewed inside the Semester Adda Unofficial app.</p>", {
                     headers: { "Content-Type": "text/html" }
                 });
             }
